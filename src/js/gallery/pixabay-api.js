1a88cc0504a0ecd16cd555b996808e6776805fcf
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-06-24 23:00:47 +0300

    добавил в gallery async/await

---
+++ src/js/gallery/pixabay-api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+// Функция запроса на бэкенд
+// export function fetchData(query) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const API_KEY = '50268892-929648ae4af930c873d247de9';
+
+//   return axios
+//     .get(BASE_URL, {
+//       params: {
+//         key: API_KEY,
+//         q: query,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     })
+//     .then(response => {
+//       return response.data;
+//     });
+// }
+
+export async function fetchData(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '50268892-929648ae4af930c873d247de9';
+
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+  return response.data;
+}
