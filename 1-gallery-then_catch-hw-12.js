790d4897e2a563673c70f5a8831ec190e53bacb1
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-07-02 16:26:27 +0000

    Deploying to gh-pages from @ DmitryKishkanev/goit-js-hw-12@c41bf9251c4fdce514b223bac25d59775cfd4d36 🚀

--- 1-gallery-then_catch-hw-12.js
+++ 1-gallery-then_catch-hw-12.js
@@ -1,27 +1,27 @@
-import"./assets/styles-C23EK1d7.js";import{a as y,S as d,i as f}from"./assets/vendor-BpUGnplp.js";function p(e,t){return y.get("https://pixabay.com/api/",{params:{key:"50268892-929648ae4af930c873d247de9",q:e,page:t,per_page:15,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(o=>o.data)}const l=document.querySelector(".gallery"),s=document.querySelector(".loader"),i=document.querySelector(".load-button"),L=new d(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});function b(e){const t=e.map(({webformatURL:a,largeImageURL:n,tags:o,likes:u,views:h,comments:g,downloads:m})=>`
+import"./assets/styles-BvmzGJGv.js";import{a as B,S as M,i as $}from"./assets/vendor-BpUGnplp.js";function u(e,t){return B.get("https://pixabay.com/api/",{params:{key:"50268892-929648ae4af930c873d247de9",q:e,page:t,per_page:15,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(l=>l.data)}const g=document.querySelector(".gallery"),d=document.querySelector(".loader"),i=document.querySelector(".load-button"),q=new M(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});function y(e){const t=e.map(({webformatURL:c,largeImageURL:h,tags:l,likes:b,views:v,comments:E,downloads:k})=>`
 <li class="gallery-item">
-  <a class="gallery-link" href="${n}">
-    <img class="gallery-image" src="${a}" alt="${o}" />
+  <a class="gallery-link" href="${h}">
+    <img class="gallery-image" src="${c}" alt="${l}" />
     <ul class="gallery-analytics">
       <li class="gallery-counters">
         <h1>Likes</h1>
-        ${u}
+        ${b}
       </li>
       <li class="gallery-counters">
         <h1>Views</h1>
-        ${h}
+        ${v}
       </li>
       <li class="gallery-counters">
         <h1>Comments</h1>
-        ${g}
+        ${E}
       </li>
       <li class="gallery-counters">
         <h1>Downloads</h1>
-        ${m}
+        ${k}
       </li>
     </ul>
   </a>
 </li>
 
-  `).join("");l.insertAdjacentHTML("beforeend",t),L.refresh()}function S(){l.innerHTML=""}function E(){s.classList.add("active")}function $(){s.classList.remove("active")}function q(){i.classList.remove("is-hidden")}function v(){i.classList.add("is-hidden")}const c=document.querySelector(".form");c.addEventListener("submit",w);let r=1;v();function w(e){e.preventDefault(),S(),E();const t=e.currentTarget.elements["search-text"].value.trim();p(t,r).then(a=>{a.hits.length===0&&B("Sorry, there are no images matching your search query. Please try again!","pink"),b(a.hits),r+=1,q(),c.reset()}).catch(a=>{console.log(a)}).finally(()=>{$()})}function B(e,t){return f.show({message:e,messageColor:"#ff0000",backgroundColor:t,icon:"	fa fa-ban",iconColor:"#8b0000",position:"topRight"})}
+  `).join("");g.insertAdjacentHTML("beforeend",t),q.refresh()}function w(){g.innerHTML=""}function m(){d.classList.add("active")}function f(){d.classList.remove("active")}function p(){i.classList.remove("is-hidden")}function a(){i.classList.add("is-hidden")}const L=document.querySelector(".form");L.addEventListener("submit",_);i.addEventListener("click",x);let r="",o=1,n=0,s=0;a();function _(e){e.preventDefault(),w(),a(),m(),A(),r=e.currentTarget.elements["search-text"].value.trim(),u(r,o).then(t=>{t.hits.length===0&&S("Sorry, there are no images matching your search query. Please try again!","pink"),y(t.hits),p(),o+=1,s=t.totalHits,n+=t.hits.length,L.reset()}).catch(t=>{console.log(t)}).finally(()=>{f()})}function x(){a(),m(),u(r,o).then(e=>{n+=e.hits.length,o+=1,y(e.hits),n>=s?(S("Sorry, but you have reached the end of the search results","pink"),a()):p()}).catch(e=>{console.log(e)}).finally(()=>{f()})}function A(){o=1,n=0,s=0}function S(e,t){return $.show({message:e,messageColor:"#ff0000",backgroundColor:t,icon:"	fa fa-ban",iconColor:"#8b0000",position:"topRight"})}
 //# sourceMappingURL=1-gallery-then_catch-hw-12.js.map
