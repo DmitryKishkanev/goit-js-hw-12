bb3e2f1f0511211519f4ca9b21384934b8a03378
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-07-03 20:40:06 +0300

    Переделал функционал на класс

--- src/js/gallery-then_catch-hw-12/gallery-main.js
+++ src/js/gallery-then_catch-hw-12/gallery-main.js
@@ -1,7 +1,8 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { getImagesByQuery } from './pixabay-api';
+// import {getImagesByQuery} from './pixabay-api';
+import GetImagesByQuery from './pixabay-api';
 import {
   createGallery,
   clearGallery,
@@ -19,20 +20,8 @@ const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', onFormSubmit);
 loadMoreButton.addEventListener('click', onLoadMoreButton);
 
-// Перменная хранит объект состояний
-const searchState = {
-  query: '',
-  page: 1,
-  totalLoaded: 0,
-  totalHits: 0,
-};
-
-// Функция сброса состояний
-function resetSearch() {
-  searchState.page = 1;
-  searchState.totalLoaded = 0;
-  searchState.totalHits = 0;
-}
+// Создаём экземпляр класса
+const getImagesByQuery = new GetImagesByQuery();
 
 // Скрываем кнопку load more
 hideLoadMoreButton();
@@ -47,14 +36,16 @@ function onFormSubmit(evt) {
   hideLoadMoreButton();
   // Показываем Loader
   showLoader();
-  // Cбрасываем счётчики
-  resetSearch();
+  // Cбрасываем объект состояний
+  getImagesByQuery.resetPage();
 
   // Сохраняем в объект состояний данные из инпута
-  searchState.query = evt.currentTarget.elements['search-text'].value.trim();
+  getImagesByQuery.query =
+    evt.currentTarget.elements['search-text'].value.trim();
 
-  // Обрабатываем промис функции запроса на бэкенд
-  getImagesByQuery(searchState.query, searchState.page)
+  // Обрабатываем промис функции запроса на бэкенд из экземпляра класса
+  getImagesByQuery
+    .fetchArticles()
     .then(data => {
       // Если ответ пришёл с ошибкой
       if (data.hits.length === 0) {
@@ -71,11 +62,10 @@ function onFormSubmit(evt) {
       showLoadMoreButton();
 
       // Сохраняем  в объект состояний кол-во доступных элементов
-      searchState.totalHits = data.totalHits;
+      getImagesByQuery.totalHits = data.totalHits;
       // Сохраняем  в объект состояний общее кол-во загруженных элементов
-      searchState.totalLoaded += data.hits.length;
-      // Сохраняем  в объект состояний увеличение номера группы на 1
-      searchState.page += 1;
+      getImagesByQuery.totalLoaded += data.hits.length;
+
       // Очищаем инпут
       formEl.reset();
     })
@@ -95,18 +85,17 @@ function onLoadMoreButton() {
   // Показываем Loader
   showLoader();
 
-  // Функция запроса дополнительной загрузки
-  getImagesByQuery(searchState.query, searchState.page)
+  // Обрабатываем промис функции запроса на бэкенд за дополнительной загрузкой
+  getImagesByQuery
+    .fetchArticles()
     .then(data => {
       // Добавляем в галерю данные к уже существующим
       createGallery(data.hits);
       // Обновляем в объекте состояний кол-во загруженных элементов
-      searchState.totalLoaded += data.hits.length;
-      // Увеличиваем в объекте состояний номера группы загружаемых элементов на 1
-      searchState.page += 1;
+      getImagesByQuery.totalLoaded += data.hits.length;
 
       // Проверяем не превышает ли кол-во загруженных элементов кличества доступных
-      if (searchState.totalLoaded >= searchState.totalHits) {
+      if (getImagesByQuery.totalLoaded >= getImagesByQuery.totalHits) {
         // Отображаем нотификацию, если всё загружено
         iziToastOptions(
           'Sorry, but you have reached the end of the search results',
@@ -127,6 +116,115 @@ function onLoadMoreButton() {
       hideLoader();
     });
 }
+
+// // Перменная хранит объект состояний
+// const searchState = {
+//   query: '',
+//   page: 1,
+//   totalLoaded: 0,
+//   totalHits: 0,
+// };
+
+// // Функция сброса состояний
+// function resetSearch() {
+//   searchState.page = 1;
+//   searchState.totalLoaded = 0;
+//   searchState.totalHits = 0;
+// }
+
+// // Скрываем кнопку load more
+// hideLoadMoreButton();
+
+// // Функция - обработчик слушателя события формы
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   // Очищаем галерею
+//   clearGallery();
+//   // Скрываем кнопку load more
+//   hideLoadMoreButton();
+//   // Показываем Loader
+//   showLoader();
+//   // Cбрасываем счётчики
+//   resetSearch();
+
+//   // Сохраняем в объект состояний данные из инпута
+//   searchState.query = evt.currentTarget.elements['search-text'].value.trim();
+
+//   // Обрабатываем промис функции запроса на бэкенд
+//   getImagesByQuery(searchState.query, searchState.page)
+//     .then(data => {
+//       // Если ответ пришёл с ошибкой
+//       if (data.hits.length === 0) {
+//         // Вызов нотификации
+//         iziToastOptions(
+//           'Sorry, there are no images matching your search query. Please try again!',
+//           'pink'
+//         );
+//         return;
+//       }
+//       // Создаём галерю с данными из бэкенда
+//       createGallery(data.hits);
+//       // Отображаем кнопку load more
+//       showLoadMoreButton();
+
+//       // Сохраняем  в объект состояний кол-во доступных элементов
+//       searchState.totalHits = data.totalHits;
+//       // Сохраняем  в объект состояний общее кол-во загруженных элементов
+//       searchState.totalLoaded += data.hits.length;
+//       // Сохраняем  в объект состояний увеличение номера группы на 1
+//       searchState.page += 1;
+//       // Очищаем инпут
+//       formEl.reset();
+//     })
+//     .catch(error => {
+//       console.log('Ошибка при загрузке изображений:', error);
+//     })
+//     .finally(() => {
+//       // Скрываем Loader
+//       hideLoader();
+//     });
+// }
+
+// // Функция - обработчик слушателя события кнопки load more
+// function onLoadMoreButton() {
+//   // Скрываем кнопку load more
+//   hideLoadMoreButton();
+//   // Показываем Loader
+//   showLoader();
+
+//   // Функция запроса дополнительной загрузки
+//   getImagesByQuery(searchState.query, searchState.page)
+//     .then(data => {
+//       // Добавляем в галерю данные к уже существующим
+//       createGallery(data.hits);
+//       // Обновляем в объекте состояний кол-во загруженных элементов
+//       searchState.totalLoaded += data.hits.length;
+//       // Увеличиваем в объекте состояний номера группы загружаемых элементов на 1
+//       searchState.page += 1;
+
+//       // Проверяем не превышает ли кол-во загруженных элементов кличества доступных
+//       if (searchState.totalLoaded >= searchState.totalHits) {
+//         // Отображаем нотификацию, если всё загружено
+//         iziToastOptions(
+//           'Sorry, but you have reached the end of the search results',
+//           'pink'
+//         );
+//         // Скрываем кнопку load more, если всё загружено
+//         hideLoadMoreButton();
+//       } else {
+//         // Иначе отображаем кнопку load more снова
+//         showLoadMoreButton();
+//       }
+//     })
+//     .catch(error => {
+//       console.log('Ошибка при загрузке дополнительных изображений:', error);
+//     })
+//     .finally(() => {
+//       // Скрываем Loader
+//       hideLoader();
+//     });
+// }
 
 // Опции подключенного через библиотеку alert
 function iziToastOptions(message, backgroundColor) {
--- src/js/gallery-then_catch-hw-12/pixabay-api.js
+++ src/js/gallery-then_catch-hw-12/pixabay-api.js
@@ -1,21 +1,94 @@
 import axios from 'axios';
 
-// Функция запроса на бэкенд
-export function getImagesByQuery(query, page) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '50268892-929648ae4af930c873d247de9';
+// Переменная для хранения базового URL
+const BASE_URL = 'https://pixabay.com/api/';
+// Переменная для хранения APY KEY
+const API_KEY = '50268892-929648ae4af930c873d247de9';
 
-  return axios
-    .get(BASE_URL, {
-      params: {
-        key: API_KEY,
-        q: query,
-        page: page,
-        per_page: 100,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => response.data);
+// Класс запроса на бэкенд
+export default class GetImagesByQuery {
+  // Конструктор для хранения объекта состояний
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.searchTotalLoaded = 0;
+    this.searchTotalHits = 0;
+  }
+
+  // Функция запроса на бэкенд
+  fetchArticles() {
+    return axios
+      .get(BASE_URL, {
+        params: {
+          key: API_KEY,
+          q: this.query,
+          page: this.page,
+          per_page: 15,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+        },
+      })
+      .then(response => {
+        this.incrementPage();
+
+        return response.data;
+      });
+  }
+  // Метод увеличения номера группы загружаемых данных на 1
+  incrementPage() {
+    this.page += 1;
+  }
+
+  // Метод сброса состояний
+  resetPage() {
+    this.page = 1;
+    this.searchTotalLoaded = 0;
+    this.searchTotalHits = 0;
+  }
+
+  // Геттеры и сеттеры для записи и чтения состояний конструктора
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get totalLoaded() {
+    return this.searchTotalLoaded;
+  }
+
+  set totalLoaded(newTotalLoaded) {
+    this.searchTotalLoaded = newTotalLoaded;
+  }
+
+  get totalHits() {
+    return this.searchTotalHits;
+  }
+
+  set totalHits(newtotalHits) {
+    this.searchTotalHits = newtotalHits;
+  }
 }
+
+// // Функция запроса на бэкенд
+// export function getImagesByQuery(query, page) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const API_KEY = '50268892-929648ae4af930c873d247de9';
+
+//   return axios
+//     .get(BASE_URL, {
+//       params: {
+//         key: API_KEY,
+//         q: query,
+//         page: page,
+//         per_page: 15,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     })
+//     .then(response => response.data);
+// }
